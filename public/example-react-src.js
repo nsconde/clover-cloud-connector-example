1b6eac7d5cc711a377f3b7f6553f4ec26b2338b0
mike hampton <mike@clover.com>
2016-07-08 15:11:50 -0600

    Cleanup and docs.

--- public/example-react-src.js
+++ public/example-react-src.js
@@ -1,54 +1,118 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var urlUtility = require('url');
+var $ = require('jquery');
+
+// This is the reusable component that can be used to configure a ICloverConnector instance
 var ConfigureApp = React.createFactory(require('remote-pay-cloud-connector-configuration-react'));
 
+// The example payment application in this repository
 var ManualTransactionApp = React.createFactory(require('./clover-sale-react-src.js'));
-// var KeyHandlerExample = React.createFactory(require('./keyhandler-example.js'));
 
+// Use the window URL to get an optional custom clientId (this will affect authorization), and
+// a friendly id to use when talking to the device.  If left unspecified, the clientId defaults
+// to the application on the https://dev1.dev.clover.com environment named 'Clover Cloud Connector Example - Heroku'.
+// This allows the deployment of this application using the automated github-heroku integration.
 var currentWindowUrl = urlUtility.parse(window.location.href, true);
 
 // If the clientId is not specified on the url, it will default to the heroku app.
 var clientId = currentWindowUrl.query['clientId'] ? currentWindowUrl.query['clientId'] : "V1T1VKQC8Y65Y";
-var friendlyId = currentWindowUrl.query['friendlyId'] ? currentWindowUrl.query['friendlyId'] : "Test " + Math.floor(Math.random() * 1000000);
+// If the friendlyId is not specified on the url, it will default to a generated string
+var friendlyId = currentWindowUrl.query['friendlyId'] ? currentWindowUrl.query['friendlyId'] : "Code " + Math.floor(Math.random() * 1000000);
 
-var appElement = document.getElementById('mikes_container');
+// This gets the html element from the webpage to render onto.
+var appElement = document.getElementById('clover_manualtransaction_container');
 
-var ConfiguredSaleApp = React.createClass({
-    displayName: "ConfiguredSaleApp",
+/**
+ * The Example application.  Ties the component that configures the connection to the
+ * Clover device to the ManualTransactionApp, which uses the ICloverConnector.
+ */
+var ConfiguredManualTransactionApp = React.createClass({
+    displayName: "ConfiguredManualTransactionApp",
 
+    /**
+     * Effectively the constructor.
+     * @returns {{cloverConnector: null}}
+     */
     getInitialState: function () {
+        // Close the connection cleanly on exit.  This should be done with all connectors.
+        $(window).on('beforeunload ', this.disposeOfResources.bind(this));
         return {
             cloverConnector: null
         }
     },
 
+    disposeOfResources: function() {
+        try {
+            if(this.state.cloverConnector) {
+                this.state.cloverConnector.dispose();
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    },
+
+    /**
+     *
+     * @param {ICloverConnector} cloverConnector
+     */
     setCloverInstance: function(cloverConnector) {
         this.setState({cloverConnector: cloverConnector})
+    },
+
+    /**
+     *
+     * @param {ConfigureApp} configureApp
+     */
+    startConfigurationApp: function(configureApp) {
+        this.disposeOfResources();
     },
 
     render: function render() {
         return (
           <div>
               <ConfigureApp
-                clientId={this.props.clientId}             // the Clover app id
-                friendlyId={this.props.friendlyId}         // the identification used when connecting to the device, and when persisting configuration
-                serverUrl={this.props.serverUrl} // used to load available clover servers
+                clientId={this.props.clientId}      // the Clover app id
+                friendlyId={this.props.friendlyId}  // the identification used when connecting to the device, and when persisting configuration
+                serverUrl={this.props.serverUrl}    // used to load available clover servers
                 configUrl={this.props.configUrl}    // used to load/save a selected configuration
                 onDeviceVerified={this.setCloverInstance}
+                onOpen={this.startConfigurationApp}
+
+                // Example of customizing the text on the configuration component
+                openButtonText="Configure Clover Device"
+                connToDeviceEstablished="Connection to device established. Getting merchant configuration information..."
+                merchConfigRetrvd="Merchant configuration information retrieved."
+                devConnToFrndlyId="Connected to "
+                posConnToFrndlyId_1="Displaying connection to "
+                posConnToFrndlyId_2=" on device."
+                posDevVerifiedCB="Device is configured.  Select 'Continue' to return to the application."
+                posCloseDev="Closing device connection."
+                posDevDisconnected="Device disconnected."
+                verifyCommButton="Verify Device Communication"
+                saveConfigButton="Save configuration"
+                closeButton="Continue"
+                title="Clover Device Configuration"
               />
-              <ManualTransactionApp taxRate={this.props.taxRate} cloverConnector={this.state.cloverConnector}/>
+              <ManualTransactionApp
+                taxRate={this.props.taxRate}
+                cloverConnector={this.state.cloverConnector}/>
           </div>
         );
     }
 });
 
+
+/**
+ * Render the application
+ *
+ */
 ReactDOM.render(
-  <ConfiguredSaleApp
+  <ConfiguredManualTransactionApp
     clientId={clientId}             // the Clover app id
     friendlyId={friendlyId}         // the identification used when connecting to the device, and when persisting configuration
     serverUrl="./data/servers.json" // used to load available clover servers
     configUrl="./configuration/"    // used to load/save a selected configuration
-    taxRate={0.11}
+    taxRate={0.0825}                // Arbitrary tax rate
   />,
   appElement);
